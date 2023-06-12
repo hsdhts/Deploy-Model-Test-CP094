c34af2565c4b335eb6251fcdc87378d8e3f45f53
husadahts <husadahutasoit@gmail.com>
2023-06-12 18:01:37 +0000

    deploy model new

--- main.py
+++ main.py
@@ -4,20 +4,8 @@ import uvicorn
 import os
 
 # Tensorflow
-from tensorflow.keras.models import load_model
-from tensorflow.keras.utils import get_file
-from tensorflow.keras.utils import load_img
-from tensorflow.keras.utils import img_to_array
-import tensorflow_decision_forests
 import tensorflow as tf
-
-from tensorflow import expand_dims
-from tensorflow.nn import softmax
 import numpy as np
-from numpy import argmax
-from numpy import max
-from numpy import array
-from json import dumps
 
 app = FastAPI()
 
@@ -34,27 +22,26 @@ app.add_middleware(
 )
 
 
-
 @app.get("/")
 async def root():
-    return {"message": "Welcome !"}
+    return {"message": "Welcome!"}
 
 
-@app.get("/cek-model")
+@app.post("/cek_model")
 async def get_net_image_prediction():
     model = tf.saved_model.load('./model/crop_recommdation')
 
     input_data = {
-        "N": np.array([107]),
-        "P": np.array([35]),
-        "K": np.array([33]),
-        "temperature": np.array([27]),
-        "humidity": np.array([66]),
-        "ph": np.array([7]),
-        "rainfall": np.array([175])
+        "N": np.array([107], dtype=np.float32),
+        "P": np.array([35], dtype=np.float32),
+        "K": np.array([33], dtype=np.float32),
+        "temperature": np.array([27], dtype=np.float32),
+        "humidity": np.array([66], dtype=np.float32),
+        "ph": np.array([7], dtype=np.float32),
+        "rainfall": np.array([175], dtype=np.float32)
     }
 
-    pred = model.serve(input_data)
+    pred = model(input_data)
 
     # Daftar nama kelas
     class_names = ['rice', 'maize', 'chickpea', 'kidneybeans', 'pigeonpeas', 'mothbeans', 'mungbean', 'blackgram',
@@ -62,10 +49,8 @@ async def get_net_image_prediction():
                    'orange', 'papaya', 'coconut', 'cotton', 'jute', 'coffee']
 
     # Mencari indeks dengan nilai prediksi tertinggi
-
     predicted_label = pred[0]
     predicted_index = np.argmax(predicted_label)
-
 
     # Mengambil nama kelas yang sesuai dengan indeks
     predicted_class = class_names[predicted_index]
@@ -77,4 +62,4 @@ async def get_net_image_prediction():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
